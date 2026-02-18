921ff697f09d26336d85057206edcde2e039301d
Vishwas K Singh <vishwas@cloudthat.com>
2026-02-18 10:19:09 +0530

    Array Destructuring

---
+++ Frontend-Basics/app14/script1.js
@@ -0,0 +1,7 @@
+const userData = ['Vishwas K Singh', 2500];
+const [uname, salary, city] = userData;
+
+console.log(`User Name: ${uname}`);
+console.log(`Salary: ${salary}`);
+console.log(`City: ${city}`);
+
