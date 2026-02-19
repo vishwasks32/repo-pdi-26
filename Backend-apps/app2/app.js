4514dcf3c231407e0c01f703f9b51b7e1f3553b6
Vishwas K Singh <vishwas@cloudthat.com>
2026-02-19 12:24:13 +0530

    Added comments to file organizer

--- Backend-apps/app2/app.js
+++ Backend-apps/app2/app.js
@@ -13,24 +13,30 @@ const path = require('path');
 
 async function organizeFolder(folderName) {
     try {
+        // target directory in the current folder itself
         const targetDir = path.join(__dirname, folderName);
 
+        // Reading files in target drectory
         const files = await fs.readdir(targetDir);
         for (const file of files){
+
+            // Full path for eah file
             const oldPath = path.join(targetDir, file);
             const stats = await fs.lstat(oldPath);
-
-            console.log('old path status', file,stats);
+            //console.log('old path status', file,stats);
+            // if its a file ignore and continue
             if (stats.isDirectory()){
                 console.log(`${file} is a Directory`);
                 continue;
             }
 
+            // extract the extension
             const ext = path.extname(file).slice(1) || 'others';
+            // Create a folder based on extension
             const categoryDir = path.join(targetDir, ext);
-
             await fs.mkdir(categoryDir, {recursive: true});
 
+            // create a new path and move (rename)
             const newPath = path.join(categoryDir, file);
             await fs.rename(oldPath, newPath);
 
