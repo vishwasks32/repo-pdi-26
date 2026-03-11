f4dfe46e276e9fc9f9a542b365ff3ca880ea22c5
Vishwas K Singh <vishwas@cloudthat.com>
2026-03-11 10:54:31 +0530

    Hello World App in C#

---
+++ csharpapps/HelloWorld/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MyNameSpace
+{
+    class MyClass
+    {
+        public void MyMethod()
+        {
+            Console.WriteLine("Hello World from MyNamespace");
+        }
+    }
+    
+}
+
+class AnotherClass
+{
+    static void Main()
+    {
+        MyNameSpace.MyClass obj = new MyNameSpace.MyClass();
+        obj.MyMethod();
+    }
+}
